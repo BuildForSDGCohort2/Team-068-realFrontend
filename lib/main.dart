b08defec3bec1e9082e747f0d4eaad5c8d5ea071
heznite <odunolatubosun@gmail.com>
2020-09-16 13:10:34 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:lacus/ui/views/delivery_details/delivery_details.dart';
 import 'package:lacus/ui/views/prices/prices.dart';
 import 'package:lacus/ui/views/signin/signin.dart';
 import 'package:lacus/ui/views/signup/signup.dart';
-import 'package:lacus/ui/views/signup/signup_successful.dart';
+import 'package:lacus/ui/views/signup/signup_success/signup_successful.dart';
 import 'package:lacus/ui/views/splashscreen/background.dart';
 import 'package:lacus/ui/views/splashscreen/welcome.dart';
 import 'package:lacus/ui/views/trip_details/trip_details.dart';
